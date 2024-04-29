d829c1dcf603612a90a86e66a37ea013e2b7658e
Gorbunov Ivan <falseprogrammerfirst@gmail.com>
2024-04-29 11:55:38 +0300

    Update install_django_empty.sh

--- install_django_empty.sh
+++ install_django_empty.sh
@@ -195,26 +195,18 @@ def get_secret_key(): \
 /g' ./src/settings/settings/base_settings.py
 
 echo '  --  2'
-sed -i 's/SECRET_KEY = /SECRET_KEY = get_secret_key()   # \
-/g' ./src/settings/settings/base_settings.py
+sed -i 's/SECRET_KEY = /SECRET_KEY = get_secret_key()   #/g' ./src/settings/settings/base_settings.py
 
 echo '  --  3'
-sed -i 's/SECRET_KEY = /SECRET_KEY = get_secret_key()   # \
-/g' ./src/settings/settings/base_settings.py
-
-echo '  --  4'
-sed -i 's/BASE_DIR = Path(__file__).resolve().parent.parent/ \
-BASE_DIR = Path(__file__).resolve().parent.parent.parent \
+sed -i 's/BASE_DIR = Path(__file__).resolve().parent.parent/BASE_DIR = Path(__file__).resolve().parent.parent.parent \
 ROOT_DIR = environ.Path(__file__) - 3 \
 /g' ./src/settings/settings/base_settings.py
 
-echo '  --  5'
-sed -i 's/DEBUG = True/DEBUG = env.bool("DEBUG", False) \
-/g' ./src/settings/settings/base_settings.py
+echo '  --  4'
+sed -i 's/DEBUG = True/DEBUG = env.bool("DEBUG", False)/g' ./src/settings/settings/base_settings.py
 
-echo '  --  6'
-sed -i 's/ALLOWED_HOSTS = []/ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS") \
-/g' ./src/settings/settings/base_settings.py
+echo '  --  5'
+sed -i 's/ALLOWED_HOSTS = []/ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")/g' ./src/settings/settings/base_settings.py
 
 
 echo 'Creation finished successfully!'
